ce6c2331cae105460abb8135a59e8aba5a8e6753
clilc <clilc@naver.com>
2021-05-07 19:07:00 +0900

    feat: 챕터2 실습&정리

---
+++ src/main/kotlin/ch2/Eval.kt
@@ -0,0 +1,53 @@
+package ch2
+
+fun eval(e: Expr): Int {
+    if (e is Num) {
+        val n = e as Num
+        return n.value
+    }
+    if (e is Sum) {
+        return eval(e.right) + eval(e.left)
+    }
+    throw IllegalArgumentException("Unknown expression")
+}
+
+fun eval2(e: Expr): Int =
+    if (e is Num) {
+        e.value
+    } else if (e is Sum) {
+        eval(e.right) + eval(e.left)
+    } else {
+        throw IllegalArgumentException("Unknown expression")
+    }
+
+
+fun eval3(e: Expr): Int =
+    when (e) {
+        is Num ->
+            e.value
+        is Sum ->
+            eval(e.right) + eval(e.left)
+        else ->
+            throw IllegalArgumentException("Unknown expression")
+    }
+
+fun evalWithLogging(e: Expr): Int =
+    when (e) {
+        is Num -> {
+            println("num: ${e.value}")
+            e.value
+        }
+        is Sum -> {
+            val left = evalWithLogging(e.left)
+            val right = evalWithLogging(e.right)
+            println("Sum: $left + $right")
+            left + right
+        }
+        else -> throw IllegalArgumentException("Unknown Expression")
+    }
+
+fun main(args: Array<String>) {
+    println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
+    println(eval2(Sum(Num(1), Num(2))))
+    println(evalWithLogging(Sum(Num(1), Num(2))))
+}
--- src/main/kotlin/ch2/README.md
+++ src/main/kotlin/ch2/README.md
@@ -76,6 +76,45 @@ person.isMarried = false // 자바와 다르게 사용 가능
 ```
 - 프로퍼티 이름을 직접 사용해도 자동으로 게터 호출
 
+### 6. enum과 when
+- enum
+  - 자바와 다르게 enum class를 사용
+  - 값만 열거하는 존재가 아닌 프로퍼티나 메소드를 정의할 수 있음
+  - enum 클래스 안에 메소드를 정의하는 경우 반드시 ENUm 상수 목록과 메소드 정의 사이에 세미콜론을 넣어야 함
 
+- when
+  - 자바와 다르게 break를 넣지 않아도 됨
+  - 한 분기 안에서 여러 값을 매치 할 경우 콤마(,)로 분리
+  - 인자가 없는  When을 사용하면 불필요한 객체생성을 막을 수 있어 성능을 향상시키지만 코드가 읽이 어려워 지는 단점이 있음  
 
+- is
+  - 코틀린의 is는 자바의 instanceof와 비슷하다
+  - 변수가 원하는 타입인지 is로 검사하면 굳이원하는 타입으로 캐스팅 하지 않아도 사용할 수 있음(스마트 캐스트)
+  
+### 7. While과 for 루프
+- while, do while
+  - 자바와 비슷
 
+```
+for (i in 1..100) {
+
+}
+
+for (i in 100 downTo 1 step 2) {
+
+}
+```
+- for
+  - ..은 항상 범위의 우항 값을 포함
+  - downTo 1 이면 -1 만큼 작아지는 역방향 수열을 만든다
+  - step 2는 증가 값의 절대값이 2로 바뀌기 때문에 증가값은 -2
+  - map, collection에 대하여 in을 사용하여 이터레이션 가능
+  
+-in
+  - c in 'a'..'z' 를 하면 'a' <= c && c <= 'z'로  실행 해줌
+  - "Kotlin" in "Java".."Scala" 처럼 Java와 Scala 사이의 문자열인지 확인 해줌
+  - "Kotlin" in setOf("Java","Scala)도 가능
+
+### 8. 코틀린의 예외 처리
+- 자바와 다르게 throw new Exception에서 throw Exception으로 사용 가능
+- 예외 처리에서 함수 선언 뒤에 throws ~Exception을 안붙여도 됨 
